36a5e66fdffa861b619f9ff9bda0d02853b06837
Viktor Pogrebniak <avtomaton@gmail.com>
2019-03-05 20:38:17 +0300

    initial data update

--- erpfil/initial-data-ru.sql
+++ erpfil/initial-data-ru.sql
@@ -16,3 +16,23 @@ INSERT INTO partner_type (title, customer, contractor) VALUES(
 INSERT INTO partner_type (title, customer, contractor) VALUES(
   "Сервис", 0, 1
 );
+
+DELETE FROM product_unit;
+INSERT INTO product_unit (title) VALUES('м2');
+INSERT INTO product_unit (title) VALUES('м3');
+INSERT INTO product_unit (title) VALUES('упак.');
+INSERT INTO product_unit (title) VALUES('шт.');
+
+DELETE FROM partner;
+INSERT INTO partner (title, partner_type_id, manager_id) VALUES(
+  'РАНХИГС', 1, 1
+);
+INSERT INTO partner (title, partner_type_id, manager_id) VALUES(
+  'ООО "Дело Жизни"', 1, 1
+);
+INSERT INTO partner (title, partner_type_id, manager_id) VALUES(
+  'ООО "Принт-маркет"', 3, 1
+);
+INSERT INTO partner (title, partner_type_id, manager_id) VALUES(
+  'ООО "Премьер-Видео"', 3, 1
+);
